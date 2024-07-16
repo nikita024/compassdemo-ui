a5be90f9fff65c19211fe4296fadfa17f1fb0367
nikita.roy <nikitaroy024@gmail.com>
2024-07-16 17:14:42 +0530

    metric page and plan theme page

--- src/pages/settings/MetricSetting.jsx
+++ src/pages/settings/MetricSetting.jsx
@@ -1,44 +1,159 @@
-import { TabItem } from '@empuls/dsm'
-import Card from '@empuls/dsm/core/card/Card'
-import Tab from '@empuls/dsm/core/tab/Tab'
-import React from 'react'
+import { Input, TabItem } from '@empuls/dsm';
+import Tab from '@empuls/dsm/core/tab/Tab';
+import React from 'react';
+import UnionImg from '../../assets/images/Union.svg';
+import { Checkbox, CheckboxGroup } from '@empuls/dsm';
+import Button from '@empuls/dsm/core/button/Button';
 
 const MetricSetting = () => {
   return (
     <>
-    <div className="plan-theme" style={{ marginBottom: "13px"}}>
-       <div className="plan-theme-header">
-          <h2 style={{"fontSize": "18px"}}>Metrics Settings</h2>
-          <p style={{"fontSize": "16px"}}>Enter the User Inputs if you want to set up an 
-            Estimator for this plan and define the relationship between the User Inputs and the Metrics.</p>
-       </div>
-    </div>
+      <div className="plan-theme" style={{ marginBottom: "13px" }}>
+        <div className="plan-theme-header">
+          <h2 style={{ fontSize: "18px" }}>Metrics Settings</h2>
+          <p style={{ fontSize: "16px" }}>
+            Enter the User Inputs if you want to set up an Estimator for this plan and define the relationship between the User Inputs and the Metrics.
+          </p>
+        </div>
+      </div>
 
-    <div className="metric-card">
-    <Tab activeTab={0}
-         onChange={function noRefCheck(){}}
-     >
-        <TabItem title="Targets for Metrics">
-          Content 1{' '}
-        </TabItem>
-        <TabItem title="Metrics to Display">
-          Content 2{' '}
-        </TabItem>
- 
-    </Tab>
-    <div className="card-typo"style={{margin:"20px"}}>
-       <p style={{"color":"#041A2F", "fontSize": "16px", "marginBottom": "10px"}}>Themes targets will be added to the Metric Scorecard on the Compass App</p>
-        
-    <div className="card">
-       <p style={{"color":"#273241"}}>While setting a target is not mandatory, we strongly recommend it. Having a target to aim for motivates participants and helps them improve 
-       their output.We also use the Target Value to create periodic nudges and celebrations to gamify the experience for them as well.</p>
-    </div>
-  </div>
-  </div>
+      <div className="metric-card" style={{ marginBottom: "100px" }} >
+        <Tab activeTab={0} onChange={function noRefCheck() {}}
+        >
+          <TabItem title="Targets for Metrics">
+            Content 1
+          </TabItem>
+          <TabItem title="Metrics to Display">
+            Content 2
+          </TabItem>
+        </Tab>
 
-    </>
-  
-  )
+        <div className="card-typo" style={{ margin: "20px" }}>
+          <p style={{ color: "#041A2F", fontSize: "16px", marginBottom: "10px" }}>
+            Themes targets will be added to the Metric Scorecard on the Compass App
+          </p>
+
+          <div className="card">
+            <p style={{ color: "#273241" }}>
+              While setting a target is not mandatory, we strongly recommend it. Having a target to aim for motivates participants and helps them improve their output. We also use the Target Value to create periodic nudges and celebrations to gamify the experience for them as well.
+            </p>
+          </div>
+        </div>
+
+        {/* Table */}
+        <table style={{ width: '100%', borderCollapse: 'collapse' }}>
+          <thead style={{ "backgroundColor": "#F6F7F9" }}>
+            <tr style={{ borderBottom: '1px solid #CFD7E2' }}>
+              <th style={{ color: 'black', textAlign: 'left', padding: '10px' }}>Metric Name</th>
+              <th style={{ color: 'black', textAlign: 'left', padding: '10px' }}>Target Value</th>
+              <th style={{ color: 'black', textAlign: 'left', padding: '10px' }}>Visible</th>
+            </tr>
+          </thead>
+          <tbody>
+            <tr style={{ borderBottom: '1px solid #CFD7E2' }}>
+              <td style={{ padding: '10px',"color":"black" }}>
+                <img src={UnionImg} alt="metric" style={{ verticalAlign: 'middle', marginRight: '10px' }} />
+                Weighted Incentive
+              </td>
+              <td style={{ padding: '10px',"width":"10%" }}>
+                <Input placeholder='0' />
+              </td>
+              <td style={{ padding: '10px' }}>
+                <CheckboxGroup direction='column'>
+                  <Checkbox />
+                </CheckboxGroup>
+              </td>
+            </tr>
+            <tr style={{ borderBottom: '1px solid #CFD7E2' }}>
+              <td style={{ padding: '10px',"color":"black" }}>
+              <img src={UnionImg} alt="metric" style={{ verticalAlign: 'middle', marginRight: '10px' }} />
+              Health Active Percentage
+              </td>
+              <td style={{ padding: '10px',"width":"20%" }}>
+                <Input placeholder='0' />
+              </td>
+              <td style={{ padding: '10px' }}>
+                <CheckboxGroup direction='column'>
+                  <Checkbox />
+                </CheckboxGroup>
+              </td>
+            </tr>
+
+            <tr style={{ borderBottom: '1px solid #CFD7E2' }}>
+              <td style={{ padding: '10px',"color":"black" }}>
+                <img src={UnionImg} alt="metric" style={{ verticalAlign: 'middle', marginRight: '10px' }} />
+                Incremental Activ Ach Percent
+              </td>
+              <td style={{ padding: '10px',"width":"20%" }}>
+                <Input placeholder='0' />
+              </td>
+              <td style={{ padding: '10px' }}>
+                <CheckboxGroup direction='column'>
+                  <Checkbox />
+                </CheckboxGroup>
+              </td>
+              </tr>
+
+              <tr style={{ borderBottom: '1px solid #CFD7E2' }}>
+              <td style={{ padding: '10px',"color":"black" }}>
+                <img src={UnionImg} alt="metric" style={{ verticalAlign: 'middle', marginRight: '10px' }} />
+                Recruitment AchievementPercent
+              </td>
+              <td style={{ padding: '10px',"width":"20%" }}>
+                <Input placeholder='0' />
+              </td>
+              <td style={{ padding: '10px' }}>
+                <CheckboxGroup direction='column'>
+                  <Checkbox />
+                </CheckboxGroup>
+              </td>
+            </tr>
+
+            <tr>
+              <td style={{ padding: '10px',"color":"black" }}>
+                <img src={UnionImg} alt="metric" style={{ verticalAlign: 'middle', marginRight: '10px' }} />
+                Non Motor Achievement Percent
+              </td>
+              <td style={{ padding: '10px',"width":"20%" }}>
+                <Input placeholder='0' />
+              </td>
+              <td style={{ padding: '10px' }}>
+                <CheckboxGroup direction='column'>
+                  <Checkbox />
+                </CheckboxGroup>
+              </td>
+            </tr>
+          </tbody>
+        </table>
+      </div>
+
+      <div>
+        jhbjhb
+      </div>
+
+      <div className="footer">
+        <div className="left-buttons">
+        </div>
+        <div className="right-buttons">
+            <Button 
+              variant='outlined' 
+              className="button" 
+             
+            >
+                Cancel
+            </Button>
+            <Button 
+              className="button"
+             
+            >
+                Save
+            </Button>
+        </div>
+      </div>
+ </>
+           
+       
+  );
 }
 
-export default MetricSetting
+export default MetricSetting;
--- src/pages/settings/PlanTheme.jsx
+++ src/pages/settings/PlanTheme.jsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from 'react'
+import React, { useEffect, useState } from 'react';
 import MansionImg from "../../assets/images/mansion.svg";
 import SalesmanImg from "../../assets/images/salesman.svg";
 import HikingImg from "../../assets/images/hiking.svg";
@@ -17,6 +17,7 @@ const PlanTheme = () => {
    const [isOpen, setIsOpen] = useState(false);
    const [themeName, setThemeName] = useState('');
    const [isSaveButtonDisabled, setIsSaveButtonDisabled] = useState(false);
+   const [selectedTheme, setSelectedTheme] = useState(null);
 
    const handleSave = () => {
       navigate('/settings');
@@ -27,157 +28,167 @@ const PlanTheme = () => {
       setIsSaveButtonDisabled(isFormValid);
    }, [themeName]);
 
-  return (
-   <>
-      <div className="plan-theme" style={{ marginBottom: "50px"}}>
-         <div className="plan-theme-header">
-            <h2 style={{"fontSize": "18px"}}>Plan Themes</h2>
-            <p style={{"fontSize": "16px"}}>Themes customize your plan with different visuals. Change themes even while plan is running.</p>
-         </div>
+   const handleThemeSelect = (theme) => {
+      setSelectedTheme(theme);
+   };
 
-         <div className="plan-theme-card">
+   return (
+      <>
+         <div className="plan-theme" style={{ marginBottom: "50px"}}>
+            <div className="plan-theme-header">
+               <h2 style={{"fontSize": "18px"}}>Plan Themes</h2>
+               <p style={{"fontSize": "16px"}}>Themes customize your plan with different visuals. Change themes even while plan is running.</p>
+            </div>
 
-            <div className="theme-card-container">
-               <div className='theme-card'>
-                  <h3 style={{"fontSize": "14px", marginBottom: "10px"}}>Mansion</h3>
-                  <img src={MansionImg} alt="mansion" />
-               </div>
-               <div className='theme-card'>
-                  <h3 style={{"fontSize": "14px", marginBottom: "10px"}}>Salesman</h3>
-                  <img src={SalesmanImg} alt="salesman" />
-               </div>
-               <div className='theme-card'>
-                  <h3 style={{"fontSize": "14px", marginBottom: "10px"}}>Hiking</h3>
-                  <img src={HikingImg} alt="hiking" />
-               </div>
-               <div className='theme-card'>
-                  <h3 style={{"fontSize": "14px", marginBottom: "10px"}}>Car Collector</h3>
-                  <img src={CarCollectorImg} alt="Car Collector" />
-               </div>
+            <div className="plan-theme-card">
+               <div className="theme-card-container">
+                  <div
+                     className={`theme-card ${selectedTheme === 'Mansion' ? 'selected' : ''}`}
+                     onClick={() => handleThemeSelect('Mansion')}
+                  >
+                     <h3 style={{"fontSize": "14px", marginBottom: "10px"}}>Mansion</h3>
+                     <img src={MansionImg} alt="mansion" />
+                  </div>
+                  <div
+                     className={`theme-card ${selectedTheme === 'Salesman' ? 'selected' : ''}`}
+                     onClick={() => handleThemeSelect('Salesman')}
+                  >
+                     <h3 style={{"fontSize": "14px", marginBottom: "10px"}}>Salesman</h3>
+                     <img src={SalesmanImg} alt="salesman" />
+                  </div>
+                  <div
+                     className={`theme-card ${selectedTheme === 'Hiking' ? 'selected' : ''}`}
+                     onClick={() => handleThemeSelect('Hiking')}
+                  >
+                     <h3 style={{"fontSize": "14px", marginBottom: "10px"}}>Hiking</h3>
+                     <img src={HikingImg} alt="hiking" />
+                  </div>
+                  <div
+                     className={`theme-card ${selectedTheme === 'Car Collector' ? 'selected' : ''}`}
+                     onClick={() => handleThemeSelect('Car Collector')}
+                  >
+                     <h3 style={{"fontSize": "14px", marginBottom: "10px"}}>Car Collector</h3>
+                     <img src={CarCollectorImg} alt="Car Collector" />
+                  </div>
 
-               <div 
-                  className='theme-card'
-                  onClick={() => {
-                     setIsOpen(true);
+                  <div 
+                     className='theme-card'
+                     onClick={() => {
+                        setIsOpen(true);
                      }} 
-               >
-                  <img src={NewThemeImg} alt="NewTheme" style={{ marginTop: "30px" }} />
+                  >
+                     <img src={NewThemeImg} alt="NewTheme" style={{ marginTop: "30px" }} />
+                  </div>
                </div>
             </div>
-
          </div>
-      </div>
-      <div className="footer">
-        <div className="left-buttons">
-        </div>
-        <div className="right-buttons">
-            <Button 
-               variant='outlined' 
-               className="button" 
-               onClick={handleSave}
-            >
-                Cancel
-            </Button>
-            <Button 
-               className="button"  
-               onClick={handleSave}
-            >
-                Save
-            </Button>
-        </div>
-      </div>
-      <Modal 
-         isOpen={isOpen} 
-         onClose={() => {
-            setIsOpen(false);
-         }} 
-         
-         padding={false} 
-         disableCloseButton={false} 
-         transitionDirection='left'
-         width='550px'
-         height='100%'
-         style={{
-            position: 'fixed',
-            top: 0,
-            right: 0,
-            display: 'flex',
-            alignItems: 'center',
-            justifyContent: 'center',
-            overflowY: 'auto',
-            overflowX: 'hidden',
-         }}
-      >
-         <Modal.Header>
-            <Typography 
-               variant='h3' 
-               component='h3'
-               style={{ color: '#000' }}
-            >
-               Create Theme
-               </Typography>
-         </Modal.Header>
-         <Modal.Body>
-            <div style={{ width: '100%' }}>
-               <div>
-                  <Label style={{ color: '#041A2F', fontSize: '14px', fontWeight: '500' }}>Theme Name*</Label>
-                  <Input
-                     className="my-class"
-                     onBlur={function noRefCheck() { }}
-                     onChange={(e) => setThemeName(e.target.value)}
-                     onFocus={function noRefCheck() { }}
-                     onKeyPress={function noRefCheck() { }}
-                     placeholder="Placeholder Text"
-                     required
-                  />
-               </div>
-               
-               <p style={{ color: '#394960', fontSize: '14px', marginTop: '20px' }}>Select Background</p>
-                <div className="theme-img" style={{ marginTop: '20px',display: 'flex', gap:'20px'}}>
-                  <img src={manImg} alt="theme" />
-                  <img src={salesImg} alt="theme" />
-                  <img src={collectImg} alt="theme" />
-                  <img src={hikeImg} alt="theme" />
-               </div>
-                   <p style={{ color: '#394960', fontSize: '14px', marginTop: '20px' }}>Images must be in .PNG or .JPG format and 150 x 150px in size</p>
-
-                <div className="main-frame">
-                        <img src={mainImg} alt="theme" />
-                  </div>   
-
+         <div className="footer">
+            <div className="left-buttons">
             </div>
-         </Modal.Body>
-         <Modal.Footer
-            style={{
-               position: "relative",
-            }}
-         >
-            <div style={{
-               textAlign: 'right'
-            }}>
+            <div className="right-buttons">
                <Button 
-                  variant='outlined'
-                  onClick={() => {
-                     setIsOpen(false);
-                  }} 
+                  variant='outlined' 
+                  className="button" 
+                  onClick={handleSave}
                >
                   Cancel
                </Button>
                <Button 
-                  ml={2}
-                  disabled={!isSaveButtonDisabled}
-                  onClick={() => {
-                     setIsOpen(false);
-                  }} 
+                  className="button"  
+                  onClick={handleSave}
+                  disabled={!selectedTheme}
                >
-                  Submit
+                  Save
                </Button>
             </div>
-         </Modal.Footer>
-      </Modal>
-   </>
-   
-  )
-}
+         </div>
+         <Modal 
+            isOpen={isOpen} 
+            onClose={() => {
+               setIsOpen(false);
+            }} 
+            padding={false} 
+            disableCloseButton={false} 
+            transitionDirection='left'
+            width='550px'
+            height='100%'
+            style={{
+               position: 'fixed',
+               top: 0,
+               right: 0,
+               display: 'flex',
+               alignItems: 'center',
+               justifyContent: 'center',
+               overflowY: 'auto',
+               overflowX: 'hidden',
+            }}
+         >
+            <Modal.Header>
+               <Typography 
+                  variant='h3' 
+                  component='h3'
+                  style={{ color: '#000' }}
+               >
+                  Create Theme
+               </Typography>
+            </Modal.Header>
+            <Modal.Body>
+               <div style={{ width: '100%' }}>
+                  <div>
+                     <Label style={{ color: '#041A2F', fontSize: '14px', fontWeight: '500' }}>Theme Name*</Label>
+                     <Input
+                        className="my-class"
+                        onBlur={function noRefCheck() { }}
+                        onChange={(e) => setThemeName(e.target.value)}
+                        onFocus={function noRefCheck() { }}
+                        onKeyPress={function noRefCheck() { }}
+                        placeholder="Placeholder Text"
+                        required
+                     />
+                  </div>
+                  
+                  <p style={{ color: '#394960', fontSize: '14px', marginTop: '20px' }}>Select Background</p>
+                  <div className="theme-img" style={{ marginTop: '20px', display: 'flex', gap:'20px'}}>
+                     <img src={manImg} alt="theme" />
+                     <img src={salesImg} alt="theme" />
+                     <img src={collectImg} alt="theme" />
+                     <img src={hikeImg} alt="theme" />
+                  </div>
+                  <p style={{ color: '#394960', fontSize: '14px', marginTop: '20px' }}>Images must be in .PNG or .JPG format and 150 x 150px in size</p>
 
-export default PlanTheme
+                  <div className="main-frame">
+                     <img src={mainImg} alt="theme" />
+                  </div>   
+               </div>
+            </Modal.Body>
+            <Modal.Footer
+               style={{
+                  position: "relative",
+               }}
+            >
+               <div style={{ textAlign: 'right' }}>
+                  <Button 
+                     variant='outlined'
+                     onClick={() => {
+                        setIsOpen(false);
+                     }} 
+                  >
+                     Cancel
+                  </Button>
+                  <Button 
+                     ml={2}
+                     disabled={!isSaveButtonDisabled}
+                     onClick={() => {
+                        setIsOpen(false);
+                     }} 
+                  >
+                     Submit
+                  </Button>
+               </div>
+            </Modal.Footer>
+         </Modal>
+      </>
+   );
+};
+
+export default PlanTheme;
